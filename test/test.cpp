b82a71d15c3c30eab6cb2e8d78a71161b1a6e771
DelandHan <neil.deland@hotmail.com>
2016-09-30 17:09:03 +0800

    done

--- test/test.cpp
+++ test/test.cpp
@@ -4,15 +4,62 @@
 #include "stdafx.h"
 #include "..\main.h"
 #include <iostream>
+#include <fstream>
+using namespace std;
+
+void work(xml::XMLNode *source, xml::XMLNode * re)
+{
+	xml::XMLNode* node = source;
+	while (node!=source->getNextByChain(false))
+	{
+		if (node->getString() == "TD")
+		{
+			xml::XMLNode * child = node->getFirstChild();
+			while (child)
+			{
+				re->append(child->clone());
+				child = child->getNext();
+			}
+			node = node->getNextByChain();
+		}
+		else 
+			node = node->getNextByChain();
+	}
+}
 
 int main()
 {
 	xml::XMLParser xp;
 
 	xp.parseFile("iecu.xml");
-	xml::XMLNode * node = xp.pickupDocument();
-	xp.saveNode(node, &std::cout);
+	xml::XMLNode * docu = xp.pickupDocument();
 
+	xml::XMLNode *re = new xml::XMLNode;
+	xml::XMLNode* node = docu;
+
+	while (node)
+	{
+
+		if (node->getString() == "table")
+		{
+			work(node, re);
+			xml::XMLNode *cl = re->clone();
+			node->insert(cl, false);
+			delete re;
+			re = new xml::XMLNode;
+			xml::XMLNode * temp = node;
+			node = node->getNextByChain(false);
+			delete temp;
+		}
+		else
+		{
+			node = node->getNextByChain();
+		}
+	}
+	
+	std::ofstream of("out.xml");
+	xp.saveNode(docu, &of);
+	of.close();
 
 	system("pause");
     return 0;
